0371f2c13c93abda2d1de81e4726d617f8926321
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-02-14 01:12:14 +0700

    require body-parser in app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const routes = require('./routes')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', routes)
 

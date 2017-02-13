96d77a5ed624cb171d377345c18dae89bb377eee
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-02-14 01:55:21 +0700

    require mongoose and add mongolab credential in app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+const mongoose = require('mongoose')
 const routes = require('./routes')
+const configDB = require('./credentials/mongolab.js')
+const port = process.env.PORT || 3000
 
 const app = express()
+
+mongoose.connect(configDB.url)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
---
+++ credentials/mongolab.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url' : 'mongodb://yourusername:yourpassword@yoururl'
+}

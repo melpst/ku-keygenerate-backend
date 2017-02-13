687ee8ac76d87d4d7a71afe04dda25c4e0334e3e
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-02-14 01:31:43 +0700

    add routes for login (and register)

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,18 @@ const router = Router()
 
 router.get('/', (req,res) => res.send('hello, world'))
 
+router.get('/login', (req,res) => res.send('this is login page'))
+
+router.post('/login', (req,res) => {
+	res.send({
+		'username' : req.body.username
+	})
+})
+
+router.post('/register', (req,res) => {
+	res.status(201).send({
+		'username' : req.body.username
+	})
+})
+
 module.exports = router

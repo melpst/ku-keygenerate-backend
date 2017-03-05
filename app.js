052acc7310d9d9a0672c0cc9adafe89c1100757c
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-06 00:34:19 +0700

    require 'express-session' and 'morgan'

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const session = require('express-session')
 const routes = require('./routes')
 const configDB = require('./credentials/mongolab.js')
 const port = process.env.PORT || 3000
@@ -9,8 +11,14 @@ const app = express()
 
 mongoose.connect(configDB.url)
 
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(session({
+	secret: 'qwertyuiop',
+	saveUninitialized: true,
+	resave: false
+}));
 
 app.use('/', routes)
 

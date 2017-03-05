18927d9a07b6abf5ebfa543ebc8d1df5923c248d
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-03-06 00:33:52 +0700

    use session in /login to save _id for /keygen

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
 const {Router} = require('express')
 const {User} = require('../models')
+const RSA = require('node-rsa')
 
 const router = Router()
 
 router.get('/', (req,res) => res.send('hello, world'))
 
+
 router.get('/login', (req,res) => res.send('this is login page'))
+
+router.get('/keygen', (req, res) =>	{
+	
+	res.send('this is keygen page')
+})
 
 router.post('/login', (req,res) => {
 	const query = User.findOne({username : req.body.username})
 	.then((data) => {
 		if(data.password === req.body.password){
-			console.log('login successful')
-			res.status(200).redirect('/')
+			console.log(data)
+			req.session._id = data._id
+			console.log(req.session)
+			res.status(200).redirect('/keygen')
 		}
 		else{
 			res.status(422).send('wrong password')
@@ -25,11 +34,9 @@ router.post('/register', (req,res) => {
 	newUser.username = req.body.username
 	newUser.password = req.body.password
 
-	newUser.save().
-	then((data) => {
-		res.status(201).send({
-			'username' : req.body.username
-		})
+	newUser.save()
+	.then((data) => {
+		res.status(201).send(data)
 	})
 })
 

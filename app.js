84ab0c8dfba5ff960a5c417f5526655e7a9c2324
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-04 14:14:03 +0700

    add "bluebird" for making mongoose promise

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000
 const app = express()
 
 mongoose.connect(configDB.url)
+mongoose.Promise = require('bluebird')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())

30c037ca014a63c89334afe0719d4fbaff9fda3e
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-21 01:35:01 +0700

    recode using async/await instead

--- routes/subjects.js
+++ routes/subjects.js
@@ -6,24 +6,19 @@ const router = Router()
 
 router.get('/', (req,res) => res.send('subjects~~'))
 
-router.get('/:subjectId', (req, res) => {
-	User.findOne({_id: req.session._id})
-	.then((data) => {
-		axios.get('http://localhost:4000/subjects/'+req.params.subjectId, {
-			state: data.state
-		})
-		.then((response) => {
-			axios.get('http://localhost:3000/decrypt', {
-				params: {
-					id: req.session._id,
-					ciphers: data
-				}
-			})
-			.then((response) => {
-				res.send(response.data)
-			})
-		})
+router.get('/:subjectId', async (req, res) => {
+
+
+	const data = await User.findOne({_id: req.session._id})
+	const subjectsResponse = await axios.get('http://localhost:4000/subjects/'+req.params.subjectId, {
+		state: data.state
 	})
+	console.log('cipher', subjectsResponse.data)
+	const decryptResponse = await axios.post('http://localhost:3000/decrypt', {
+		_id: req.session._id,
+		ciphers: subjectsResponse.data
+	})
+	res.send(decryptResponse.data)
 	
 })
 

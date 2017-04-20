bb9d1925f23cbee5db0c6893371299c0e76ee189
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-20 19:59:14 +0700

    send request with user state to assess

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,11 +1,22 @@
 const {Router} = require('express')
+const axios = require('axios')
+const {User} = require('../models')
 
 const router = Router()
 
 router.get('/', (req,res) => res.send('subjects~~'))
 
 router.get('/:subjectId', (req, res) => {
-	res.redirect('http://localhost:4000/cipher')
+	User.findOne({_id: req.session._id})
+	.then((data) => {
+		axios.get('http://localhost:4000/subjects/'+req.params+subjectId, {
+			state: data.state
+		})
+		.then((response) => {
+			res.send(response.data)
+		})
+	})
+	
 })
 
 module.exports = router

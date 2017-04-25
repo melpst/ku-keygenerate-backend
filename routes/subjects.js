71f0bb066a3a817376ad37df9ab05e14bc17814d
Jitlada Supreeyaporn <jitlada.su@ku.th>
2017-04-25 19:10:58 +0700

    use session.state instead of get from db

--- routes/subjects.js
+++ routes/subjects.js
@@ -9,22 +9,22 @@ router.get('/', (req,res) => res.send('subjects~~'))
 
 router.get('/:subjectId', async (req, res) => {
 	let canAssess = false
-
-	console.log('req.session._id:', req.session._id)
-	console.log('req.headers._id:', req.headers._id)
-	if (req.headers._id && !req.session._id) {
-		req.session._id = req.headers._id
-	}
-	console.log('req.session._id:', req.session._id)
-	console.log('req.headers._id:', req.headers._id)
-	const data = await User.findOne({_id: req.session._id})
-	console.log('state', data.state)
+	// console.log('req.session._id:', req.session._id)
+	// console.log('req.headers._id:', req.headers._id)
+	// if (req.headers._id && !req.session._id) {
+	// 	req.session._id = req.headers._id
+	// }
+	// console.log('req.session._id:', req.session._id)
+	// console.log('req.headers._id:', req.headers._id)
+	// const data = await User.findOne({_id: req.session._id})
+	// console.log('state', data.state)
+	console.log('sned state to :4000/auth to check')
 	const subjectsResponse = await axios.get('http://localhost:4000/auth', {
-		headers: {state: data.state}
+		headers: {state: req.session.state}
 	})
 
 	if(!_.isEqual(subjectsResponse.data, {success: true})){
-		console.log('/decrypt get word')
+		console.log('state == false, /decrypt to know word')
 		const decryptResponse = await axios.post('http://localhost:3000/decrypt', {
 			_id: req.session._id,
 			ciphers: subjectsResponse.data
@@ -37,7 +37,8 @@ router.get('/:subjectId', async (req, res) => {
 
 		console.log('word is', decryptResponse.data)
 
-		if(_.isEqual(checkWordResponse.data, {success: true})){
+		if(!_.isEqual(checkWordResponse.data, {success: false})){
+			console.log('state == true if all users get same word')
 			const checkServerResponse = await axios.post('http://localhost:3000/checkcipher', {
 				_id: req.session._id,
 				word: decryptResponse.data,
@@ -45,10 +46,10 @@ router.get('/:subjectId', async (req, res) => {
 				keys: checkWordResponse.data
 			})
 			if(_.isEqual(checkServerResponse.data, {success: true})){
-				const updateData = await User.findOne({_id: req.session._id})
-				console.log('update data state', updateData.state)
+				// const updateData = await User.findOne({_id: req.session._id})
+				console.log('updated data state', req.session.state)
 				const updateSubjectsResponse = await axios.get('http://localhost:4000/auth', {
-					headers: {state: updateData.state}
+					headers: {state: req.session.state}
 				})
 				if(_.isEqual(checkServerResponse.data, {success: true})){
 					console.log('check after change state')
